e40c2572f6787e760a5651f2c52f2ab68abe8b62
Alany-b <alangimenezz14@gmail.com>
2025-11-26 10:31:04 -0300

    se implementaron las verificaciones en la ruta del usuario y se corrigio un error de importacion

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,20 +6,25 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 
 export const userRoutes = Router();
 
-// ruta para crear un usuario
-userRoutes.post("/users", createUser);
 
 // ruta para traer todos los usuarios
-userRoutes.get("/users", getAllUser);
+userRoutes.get("/users", authMiddleware, adminMiddleware,getAllUser);
+
+// ruta para crear un usuario
+userRoutes.post("/users", authMiddleware, adminMiddleware, createUser);
+
 
 // ruta para traer un solo usuario
-userRoutes.get("/users/:id", getUserById);
+userRoutes.get("/users/:id", authMiddleware, adminMiddleware, getUserById);
+
 
 // ruta para actualizar un usuario
-userRoutes.put("/users/:id", updateUser);
+userRoutes.put("/users/:id", authMiddleware, adminMiddleware, updateUser);
 
 // ruta para eliminar un usuario
 userRoutes.delete("/users/:id", deleteUser);
